7969d43ebe542677783dfb20fd9aa7d37f492d41
umbur <you@example.com>
2019-08-09 11:20:41 -0600

    created froms with formik

--- client/form/src/App.js
+++ client/form/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import RegisterForm from './components/RegisterForm.js';
 
 function App() {
   return (
     <div className="App">
-     
+     <RegisterForm />
     </div>
   );
 }
