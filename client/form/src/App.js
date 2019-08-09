acb2fab88c2e7a2f3ab63d62ed7dc78eaa790fc9
umbur <you@example.com>
2019-08-09 14:27:29 -0600

    created GetData components and used a map method to display some data

--- client/form/src/App.js
+++ client/form/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import RegisterForm from './components/RegisterForm.js';
+import GetData from './components/GetData.js';
 
 function App() {
   return (
     <div className="App">
      <RegisterForm />
+     <GetData />
     </div>
   );
 }
